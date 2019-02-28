e16f5f653473e7deb22857a4dded7320941ee0f6
Andy Bell <andy.bell1987@gmail.com>
2019-02-28 17:15:42 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlphaBlog::Application.config.secret_key_base = '9ebe3c31616dbe1dec16e373a35c8a1cef0bfe40cec1b50d0262e052bcea3e1d8ae1f90986aadcff223b37e225b30c16c8bf575dec3cdde06bc7995783422a4f'
